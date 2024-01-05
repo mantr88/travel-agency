79667a98fcf691c3c093d6c39fe8a603b7b9e378
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2024-01-05 11:42:32 +0200

    add: init files

---
+++ src/ui/svgElements/Burger.tsx
@@ -0,0 +1,17 @@
+function Burger() {
+  return (
+    <svg
+      width="40"
+      height="30"
+      viewBox="0 0 40 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0H40V3.75H0V0Z" fill="#266294" />
+      <path d="M10 13.125H30V16.875H10V13.125Z" fill="#266294" />
+      <path d="M0 26.25H40V30H0V26.25Z" fill="#266294" />
+    </svg>
+  );
+}
+
+export default Burger;
